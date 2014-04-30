74666823228129db5ecf6c76a3580fa9536da520
Olivier Van Roey <olivier.van.roey@telenet.be>
2014-04-30 19:05:47 +0200

    LoginDAO aangepast

--- src/main/java/com/programmeren4/turnahead/server/model/dao/LoginDao.java
+++ src/main/java/com/programmeren4/turnahead/server/model/dao/LoginDao.java
@@ -21,22 +21,23 @@ public class LoginDao {
 		LoginDTO LoginReturn = null;
 		ResultSet rs = null;
 		try {
-//			Class.forName(DBConnector.DRIVER_CLASS).newInstance();
-//			this.conn = DBConnector.getInstance().getConn();
-//			String sql = "SELECT * FROM USER";// WHERE EMAIL=" + login.getEMail();
-//			rs = conn.createStatement().executeQuery(sql);
-//			if (rs.next()) {
-//				LoginReturn = new LoginDTO();
-//				LoginReturn.setEMail(rs.getString("EMAIL"));
-//				LoginReturn.setPassword(rs.getString("PASSWORD"));
-//			}
-//		} catch (SQLException se) {
-//			se.printStackTrace();
+			//Class.forName(DBConnector.DRIVER_CLASS).newInstance();
+			DBConnector.getInstance().init();
+			this.conn = DBConnector.getInstance().getConn();
+			String sql = "SELECT * FROM USER";// WHERE EMAIL=" + login.getEMail();
+			rs = conn.createStatement().executeQuery(sql);
+			if (rs.next()) {
+				LoginReturn = new LoginDTO();
+				LoginReturn.setEMail(rs.getString("EMAIL"));
+				LoginReturn.setPassword(rs.getString("PASSWORD"));
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-//			DBConnector.getInstance().close(rs);
-//			DBConnector.getInstance().closeConn();
+			DBConnector.getInstance().close(rs);
+			DBConnector.getInstance().closeConn();
 		}
 
 		return LoginReturn;
